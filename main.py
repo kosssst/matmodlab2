9f0fe827e302122fe080126722271cd6551bf599
kosssst <kostyamed0003@gmail.com>
2022-10-30 17:05:51 +0300

    repaired exception

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import simplex
+
+if __name__ == "__main__":
+    c = [1, -2, 3, -1]
+    A = [[2, -1, 2, -3], [1, 2, -1, 1]]
+    b = [5, 3]
+    try:
+        t, s, v = simplex.simplex(c, A, b)
+        print(s)
+        print(v)
+    except:
+        print("Has no solutions")
+    
